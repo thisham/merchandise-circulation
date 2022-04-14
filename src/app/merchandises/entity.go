250f8863457a0a7ae1e100130e0cfd79109e87ee
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-04-14 18:40:55 +0700

    feat: add merchandise entity

---
+++ src/app/merchandises/entity.go
@@ -0,0 +1,33 @@
+package merchandises
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Domain struct {
+	Id              uuid.UUID
+	ProductionCode  string
+	Name            string
+	Stock           int
+	Price           float64
+	Description     string
+	MerchandiseType MerchandiseTypeReference
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+type MerchandiseTypeReference struct {
+	Name string
+}
+
+type Business interface {
+	CreateData(data Domain) (Domain, error)
+	GetAllData() []Domain
+}
+
+type Data interface {
+	InsertData(data Domain) (Domain, error)
+	SelectData() []Domain
+}
