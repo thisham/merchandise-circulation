6187e053c4e4d1067d4c13c479ae4b51f94ee338
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-04-24 15:35:01 +0700

    feat: batch merchandise type response

--- src/app/merchandise_types/handlers/response/dto.go
+++ src/app/merchandise_types/handlers/response/dto.go
@@ -7,16 +7,18 @@ type MerchandiseTypeResponse struct {
 	Name string `json:"name"`
 }
 
-func MapToResponse(domain *merchandise_types.Domain) MerchandiseTypeResponse {
+func MapToResponse(domain merchandise_types.Domain) MerchandiseTypeResponse {
 	return MerchandiseTypeResponse{
 		ID:   domain.ID,
 		Name: domain.Name,
 	}
 }
 
-func MapToDomain(response *MerchandiseTypeResponse) merchandise_types.Domain {
-	return merchandise_types.Domain{
-		ID:   response.ID,
-		Name: response.Name,
+func MapToBatchResponse(domain []merchandise_types.Domain) []MerchandiseTypeResponse {
+	responses := []MerchandiseTypeResponse{}
+
+	for _, merchType := range domain {
+		responses = append(responses, MerchandiseTypeResponse(merchType))
 	}
+	return responses
 }
