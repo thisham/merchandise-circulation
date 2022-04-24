6453e6feecc0bce3f6db7fe19e34e041e13299e6
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-04-25 05:23:43 +0700

    perf: add merchandise types domain mapper

--- src/app/merchandise_types/data/record.go
+++ src/app/merchandise_types/data/record.go
@@ -1,7 +1,25 @@
 package data
 
+import "merchandise-circulation-api/src/app/merchandise_types"
+
 // model
 type MerchandiseType struct {
 	ID   int
 	Name string
+}
+
+func mapToDomain(record MerchandiseType) merchandise_types.Domain {
+	return merchandise_types.Domain{
+		ID:   record.ID,
+		Name: record.Name,
+	}
+}
+
+func mapToBatchDomain(records []MerchandiseType) []merchandise_types.Domain {
+	domain := []merchandise_types.Domain{}
+
+	for _, rec := range records {
+		domain = append(domain, mapToDomain(rec))
+	}
+	return domain
 }
