bd7c71208e47918289d9c2fd07fd780ca09b03f6
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-04-24 11:12:16 +0700

    feat: add interfaces for services and data

--- src/app/merchandise_types/entity.go
+++ src/app/merchandise_types/entity.go
@@ -4,3 +4,13 @@ type Domain struct {
 	ID   int
 	Name string
 }
+
+// main clauses: GET, CREATE, AMEND, REMOVE
+type Services interface {
+	GetAllMerchandiseTypes() ([]Domain, error)
+}
+
+// main clauses: INSERT, SELECT, UPDATE, DELETE
+type Data interface {
+	SelectAllMerchandiseTypes() (Domain, error)
+}
