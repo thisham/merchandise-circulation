f1cd986874940ed3a68605de8d5371b6fdc6a92c
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-04-25 06:13:23 +0700

    feat: implement services

--- src/app/merchandises/services/usecase.go
+++ src/app/merchandises/services/usecase.go
@@ -1,19 +1,23 @@
 package services
 
-import "merchandise-circulation-api/src/app/merchandises"
+import (
+	"merchandise-circulation-api/src/app/merchandises"
+)
 
 type usecase struct {
 	merchandiseData merchandises.Data
 }
 
 // GetAllMerchandises implements merchandises.Services
-func (merch *usecase) GetAllMerchandises() ([]merchandises.Domain, error) {
-	panic("unimplemented")
+func (uc *usecase) GetAllMerchandises() ([]merchandises.Domain, error) {
+	result, err := uc.merchandiseData.SelectAllMerchandises()
+	return result, err
 }
 
 // GetMerchandiseByUPC implements merchandises.Services
-func (merch *usecase) GetMerchandiseByUPC(upc string) (merchandises.Domain, error) {
-	panic("unimplemented")
+func (uc *usecase) GetMerchandiseByUPC(upc string) (merchandises.Domain, error) {
+	result, err := uc.merchandiseData.SelectMerchandiseByUPC(upc)
+	return result, err
 }
 
 func NewService(merchandiseData merchandises.Data) merchandises.Services {
