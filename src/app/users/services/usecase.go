f4574d82cf95e9bb67d91bda8f30b4fa1ebb49b2
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-08-26 04:15:54 +0700

    feat(users): implement `users` feature's services

--- src/app/users/services/usecase.go
+++ src/app/users/services/usecase.go
@@ -1,7 +1,12 @@
 package services
 
 import (
+	"errors"
 	"merchandise-circulation-api/src/app/users"
+	"merchandise-circulation-api/src/utils"
+	"strings"
+
+	"github.com/google/uuid"
 )
 
 type usecase struct {
@@ -10,22 +15,45 @@ type usecase struct {
 
 // AttemptLogin implements users.Services
 func (uc *usecase) AttemptLogin(email string, password string) (token string, err error) {
-	panic("unimplemented")
+	domain, err := uc.repo.SelectUserOnLogin(email)
+
+	if err != nil {
+		return "", err
+	}
+
+	if !utils.HashValidate(password, domain.Password) {
+		return "", errors.New("password didn't match")
+	}
+	return utils.GenerateJwt(domain.ID.String())
 }
 
 // AttemptRegister implements users.Services
 func (uc *usecase) AttemptRegister(user users.Domain) (token string, err error) {
-	panic("unimplemented")
+	previousUser, err := uc.repo.SelectUserOnLogin(user.Email)
+	var userID string
+
+	if err != nil && !strings.Contains(err.Error(), "not found") {
+		return "", err
+	}
+
+	if previousUser.ID != uuid.Nil {
+		return "", errors.New("email is registered")
+	}
+
+	if userID, err = uc.repo.InsertUser(user); err != nil {
+		return "", err
+	}
+	return utils.GenerateJwt(userID)
 }
 
 // GetAllUsers implements users.Services
 func (uc *usecase) GetAllUsers() (users []users.Domain, err error) {
-	panic("unimplemented")
+	return uc.repo.SelectUsers()
 }
 
 // GetUserByID implements users.Services
 func (uc *usecase) GetUserByID(id string) (user users.Domain, err error) {
-	panic("unimplemented")
+	return uc.repo.SelectUserByID(id)
 }
 
 func NewService(repo users.Repositories) users.Services {
